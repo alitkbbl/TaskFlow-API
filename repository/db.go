206046e2b1610b1800e84f026c500241316a0c6a
alitkbbl <alitaghi8305@gmail.com>
2025-09-29 19:52:59 +0330

    develope services

--- repository/db.go
+++ repository/db.go
@@ -1,1 +1,24 @@
 package repository
+
+import (
+	"taskflow/config"
+	"taskflow/models"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func NewDB(cfg *config.Config) (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open(cfg.DBPath), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	// Auto migrate
+	err = db.AutoMigrate(&models.User{}, &models.Task{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
---
+++ services/task_service.go
@@ -0,0 +1,35 @@
+package services
+
+import (
+	"context"
+	"taskflow/models"
+	"taskflow/repository"
+)
+
+type TaskService struct {
+	taskRepo *repository.TaskRepository
+}
+
+func NewTaskService(taskRepo *repository.TaskRepository) *TaskService {
+	return &TaskService{taskRepo: taskRepo}
+}
+
+func (s *TaskService) CreateTask(ctx context.Context, task *models.Task) error {
+	return s.taskRepo.Create(ctx, task)
+}
+
+func (s *TaskService) GetTask(ctx context.Context, id string) (*models.Task, error) {
+	return s.taskRepo.FindByID(ctx, id)
+}
+
+func (s *TaskService) ListTasks(ctx context.Context, filters map[string]string) ([]models.Task, error) {
+	return s.taskRepo.List(ctx, filters)
+}
+
+func (s *TaskService) UpdateTask(ctx context.Context, task *models.Task) error {
+	return s.taskRepo.Update(ctx, task)
+}
+
+func (s *TaskService) DeleteTask(ctx context.Context, id string) error {
+	return s.taskRepo.Delete(ctx, id)
+}
